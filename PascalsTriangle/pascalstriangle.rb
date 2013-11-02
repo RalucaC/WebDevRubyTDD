80453b741c64a8f5ad25f8ccdaa86004a2d5995c
Raluca <raluca.constanda@gmail.com>
2013-11-02 12:25:32 +0200

    Update pascalstriangle.rb

--- PascalsTriangle/pascalstriangle.rb
+++ PascalsTriangle/pascalstriangle.rb
@@ -1,4 +1,38 @@
 def compute_row(n)
-  []
-end
+	
+	if n.class == String
+		return nil
+	end
+	if n == 1
+		return [1]
+	end
+	if n == 2
+		return [1,1]
+	end
 
+	ary = [1,1]
+		
+	while n > 2
+		aryAux = []
+		i = 0 		
+		while i < ary.size
+			if i == 0
+				aryAux.push(ary[i])
+				i = i + 1
+			else
+				aryAux << ary[i - 1] + ary[i]
+				
+				if i == ary.size - 1
+					aryAux.push(ary[i])
+				end
+				
+				i = i + 1
+			end
+		end
+		ary = aryAux.compact
+ 		n = n - 1	
+		
+	end
+
+	return aryAux	
+end
